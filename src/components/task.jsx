c10d0f2d0bd535856dc27eea2dfade98f7de33c2
jupegarnica <jupegarnica@gmail.com>
2019-04-11 13:46:30 +0200

    edit task done

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,8 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './task.scss';
 
 
+class Task extends React.Component {
+    state = {
+        newText: this.props.data.text
+    }
 
-const Task = props => <div className='task'>{props.text}</div>;
+    handleChange = (ev) => {
+        this.setState({ newText: ev.target.value });
+    }
+    isEnter = ev => {
+        if(ev.keyCode === 13) {
+            this.edit(ev)
+        }
+    }
+    edit = (ev) => {
+        let editedTask = {
+            ...this.props.data,
+            text: this.state.newText
+        }
+        this.props.updateTask(editedTask);
+        ev.target.blur();
+    }
+    render () {
+        return (
+          <div className='task'>
+
+            <input type='text' value={this.state.newText} onChange={this.handleChange}  onKeyUp={this.isEnter} onBlur={this.edit}/>
+
+            <div className='actions'>
+              <button onClick={() => this.props.deleteTask(this.props.data.id)}>delete</button>
+            </div>
+          </div>
+        );
+    }
+}
 
 export default Task;
--- src/views/board.jsx
+++ src/views/board.jsx
@@ -6,42 +6,65 @@ class Board extends Component {
   state = {
     newTaskText: '',
 
-      tasks: JSON.parse(localStorage.getItem('tasks')) || [],
+    tasks: JSON.parse(localStorage.getItem('tasks')) || [],
   };
 
   componentDidUpdate() {
-      localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
   }
 
   addTask = text => {
-    let newTask = {
-      text,
+    text = text.trim();
+    if (text) {
+      let newTask = {
+        id: Date.now(),
+        createAt: new Date(),
+        text,
+      };
+      this.setState({
+        tasks: [newTask, ...this.state.tasks],
+        newTaskText: '',
+      });
+    }
+  };
+
+  editTask = task => {
+    task = {
+      ...task,
+      updatedAt: new Date(),
     };
+
     this.setState({
-      tasks: [newTask, ...this.state.tasks],
-      newTaskText: '',
+      tasks: this.state.tasks.map(_task => (_task.id === task.id ? task : _task)),
     });
   };
+
+
+
+  removeTask = id => {
+    this.setState({
+      tasks: this.state.tasks.filter(task => task.id !== id),
+    });
+  };
+
   handleChange = ev => {
     this.setState({ newTaskText: ev.target.value });
-
-
   };
-  handleKeyUp = (ev) =>  {
-      if (ev.keyCode === 13) {
-          this.addTask(ev.target.value);
-      }
-  }
+  handleKeyUp = ev => {
+    if (ev.keyCode === 13) {
+      this.addTask(ev.target.value);
+    }
+  };
+
   render() {
     return (
       <div className='board'>
         <header>
-          <input type='text' placeholder='add task' onKeyUp={this.handleKeyUp}  onChange={this.handleChange} value={this.state.newTaskText} />
-
+          <input type='text' placeholder='add task' onKeyUp={this.handleKeyUp} onChange={this.handleChange} value={this.state.newTaskText} />
         </header>
         <main className='tasks'>
           {this.state.tasks.map(task => (
-            <Task text={task.text} />
+              <Task data={task} deleteTask={this.removeTask} key={task.id} updateTask={this.editTask} />
           ))}
         </main>
       </div>
