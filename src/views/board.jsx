66033a556a484c7f42b8a9acec60cfb199a8d81d
jupegarnica <jupegarnica@gmail.com>
2019-04-11 12:25:16 +0200

    add task functionality done

--- src/views/board.jsx
+++ src/views/board.jsx
@@ -2,20 +2,55 @@ import React, { Component } from 'react';
 import './board.scss';
 import Task from '../components/task.jsx';
 
-
 class Board extends Component {
+  state = {
+    newTaskText: '',
+
+    tasks: [
+      {
+        text: 'aprender jsx',
+      },
+      {
+        text: 'aprender react',
+      },
+      {
+        text: 'aprender react',
+      },
+    ],
+  };
+  addTask = text => {
+    let newTask = {
+      text,
+    };
+    this.setState({
+      tasks: [newTask, ...this.state.tasks],
+      newTaskText: '',
+    });
+  };
+  handleChange = ev => {
+    this.setState({ newTaskText: ev.target.value });
+
+
+  };
+  handleKeyUp = (ev) =>  {
+      if (ev.keyCode === 13) {
+          this.addTask(ev.target.value);
+      }
+  }
   render() {
     return (
       <div className='board'>
         <header>
-          <input type='text' placeholder='add task' />
+          <input type='text' placeholder='add task' onKeyUp={this.handleKeyUp}  onChange={this.handleChange} value={this.state.newTaskText} />
+          {this.state.newTaskText}
         </header>
         <main className='tasks'>
-                <Task text="aprender react" />
-                <Task text="aprender jsx"/>
+          {this.state.tasks.map(task => (
+            <Task text={task.text} />
+          ))}
         </main>
       </div>
-    )
+    );
   }
 }
 
